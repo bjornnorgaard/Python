6365ef9beae3da1a7a2a5c8c0ccd4488d63387da
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-01-19 20:42:19 +0000

    Setup devcontainer with local python interpreter

---
+++ src/calculator_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from src.calculator import Calculator
+
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_add(self):
+        result = self.calc.add(10, 5)
+        self.assertEqual(result, 15)
+
+    def test_subtract(self):
+        result = self.calc.subtract(10, 5)
+        self.assertEqual(result, 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/main.py
@@ -0,0 +1,20 @@
+import os
+
+from calculator import Calculator
+
+
+def main():
+    calc = Calculator()
+    sum_result = calc.add(10, 5)
+    print(f"The sum is: {sum_result}")
+
+    subtract_result = calc.subtract(10, 5)
+    print(f"The result of the subtraction is: {subtract_result}")
+
+    # Print value of env var SERVICE_NAME
+    service_name = os.environ.get("SERVICE_NAME")
+    print(f"Service name is: {service_name}")
+
+
+if __name__ == "__main__":
+    main()
